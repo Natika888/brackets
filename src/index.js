16a10ed6c2c9b2634f383ff6b5660807de350aec
Nataliia Krikun <58227698+Natika888@users.noreply.github.com>
2020-10-25 17:08:26 +0200

    feat: half task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // if (indexOf('|')!== -1 || indexOf('7')!== -1 || indexOf('8')!== -1 ) {
+        
+    // }
+
+
+
+    let stack = []
+    let brackets = bracketsConfig.flat().join('');
+  
+    for(let bracket of str) {
+      let bracketsIndex = brackets.indexOf(bracket)
+  
+      if (bracketsIndex === -1){
+        continue
+      }
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
 }
